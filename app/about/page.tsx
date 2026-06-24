690f5c934d6382b22abd7ba6a5511b872d508e35
juicethyn <juzzthynperez.personal@gmail.com>
2026-06-24 13:21:32 +0800

    fix: removed delay on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,5 @@
 import AboutSection from "@/components/sections/AboutSection";
 
 export default async function About() {
-	await new Promise((resolve) => setTimeout(resolve, 500)); // Simulate a delay of 0.5 seconds
-
 	return <AboutSection />;
 }
